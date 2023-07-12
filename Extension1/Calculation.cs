557466abe9351005391e45890788d63e8f3538e0
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-12 16:05:00 +0530

    Add project files.

---
+++ Extension1/Calculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension1
+{
+    public class Calculation
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+    public static class ExtensionClass
+    {
+        public static int Square(this Calculation c,int a)
+        {
+            return a * a;
+        }
+        public static int Mul(this Calculation c,int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Extension1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation cal=new Calculation();
+            
+            int result = cal.Sub(1,2);
+            Console.WriteLine(result);
+            int result1=cal.Mul(1,2);
+            Console.WriteLine(result1);
+
+
+
+
+        }  
+       
+    }
+  
+}
